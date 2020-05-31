f5c028e0785f93c91f940b8c42371e3be1cd0d56
Abzal.Tankibayev <Abzal.Tankibayev@outlook.com>
2020-05-31 19:01:10 +0600

    fix settings

--- src/controllers/StreamController.js
+++ src/controllers/StreamController.js
@@ -103,7 +103,7 @@ module.exports.open = async function(req, res){
                 // Если этот стрим сейчас не просматривается...
                 stream.consumers.push({consumerId: consumer._id, consumerStatus: 0}); // Добавить идентификатор зрителя
                 wsCurrentPort++;
-                stream.wsPort;// = wsCurrentPort;
+                stream.wsPort = 443;// = wsCurrentPort;
                 currentStream.push(stream); // Поместить стрим в массив просматриваемых сейчас стримов
                 activeConsumerStream.push({
                     streamId: stream._id,
